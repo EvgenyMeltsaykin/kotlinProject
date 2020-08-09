9b484169eb7c74a5a27234a3b70df503e1d9465d
Evgeny <e.meltsaykin@gmail.com>
2020-08-09 22:56:44 +0500

    Убрана проверка роли в ChooseActivity.kt

--- app/src/main/java/com/diplom/kotlindiplom/ChooseActivity.kt
+++ app/src/main/java/com/diplom/kotlindiplom/ChooseActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.core.view.isVisible
 import com.diplom.kotlindiplom.child.ChildMainActivity
+import com.diplom.kotlindiplom.models.FunctionsFirebase
 import com.diplom.kotlindiplom.parent.ParentMainActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -17,39 +18,9 @@ import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_choose.*
 
 class ChooseActivity : AppCompatActivity() {
-    var parent = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose)
-        val uid = FirebaseAuth.getInstance().uid
-        if (uid != null) {
-            chooseTextviewChoose.text = "Выполняется вход"
-            childButtonChoose.isVisible = false
-            parentButtonChoose.isVisible = false
-            val ref = FirebaseDatabase.getInstance().getReference("")
-            ref.child("roles").child("$uid").addListenerForSingleValueEvent(
-                object : ValueEventListener {
-                    override fun onCancelled(p0: DatabaseError) {
-                        TODO("Not yet implemented")
-                    }
-
-                    override fun onDataChange(p0: DataSnapshot) {
-                        val role = p0.getValue()
-                        if (role == "parent") {
-                            parent = true
-                            Log.d("Test", "parent")
-                        }
-                        if (role == "child") {
-                            Log.d("Test", "child")
-                            parent = false
-                        }
-                        checkRoleUser(parent)
-                    }
-
-                }
-
-            )
-        }
         childButtonChoose.setOnClickListener {
             intent = Intent(this, RegistryActivity::class.java)
             intent.putExtra("parentOrNot", false)
@@ -59,20 +30,6 @@ class ChooseActivity : AppCompatActivity() {
         parentButtonChoose.setOnClickListener {
             intent = Intent(this, RegistryActivity::class.java)
             intent.putExtra("parentOrNot", true)
-            startActivity(intent)
-        }
-    }
-
-    fun checkRoleUser(parent:Boolean){
-        if (parent) {
-            intent = Intent(this, ParentMainActivity::class.java)
-            intent.flags =
-                Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        }else{
-            intent = Intent(this, ChildMainActivity::class.java)
-            intent.flags =
-                Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
     }
